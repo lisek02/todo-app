ae12c9074bb4124dbba0e1bfcc4d4de0d677cdc8
Wojciech Lisiecki <wojciech.j.lisiecki@gmail.com>
2019-05-21 02:47:39 +0200

    save store to localstorage

--- src/configure-store.ts
+++ src/configure-store.ts
@@ -3,20 +3,30 @@ import { reducers } from './reducers'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
 import { createBrowserHistory } from 'history';
 import { reducer as formReducer } from 'redux-form';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 export const history = createBrowserHistory();
 
 export function configureStore() {
+  const persistConfig = {
+    key: 'todo-app',
+    storage,
+  }
+
+  
   const reducer = combineReducers({
     router: connectRouter(history),
     form: formReducer,
     ...reducers
   });
 
+  const persistedReducer = persistReducer(persistConfig, reducer);
+
   const composeEnhancers = (<any>window).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
   const store = createStore(
-    reducer,
+    persistedReducer,
     composeEnhancers(
       applyMiddleware(
         routerMiddleware(history)
@@ -24,5 +34,7 @@ export function configureStore() {
     )
   );
 
-  return store;
+  const persistor = persistStore(store)
+
+  return { store, persistor };
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,17 @@ import { Provider } from 'react-redux';
 import App from './App';
 import { configureStore, history } from './configure-store';
 import { ConnectedRouter } from 'connected-react-router';
+import { PersistGate } from 'redux-persist/integration/react'
+
+const { store, persistor } = configureStore();
 
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
